999e9d124e01ad48432726bd78e6ba9d750b4388
MattAgn <matthieua@bam.tech>
2024-12-03 22:21:37 +0100

    feat: add button to reset diary

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,11 +1,37 @@
+import { History } from "@tamagui/lucide-icons";
+import { router } from "expo-router";
+import { useAtom } from "jotai";
+import { Alert } from "react-native";
 import { Colors } from "react-native/Libraries/NewAppScreen";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { styled, Text } from "tamagui";
+import { Button, styled, Text } from "tamagui";
+
+import { diaryEntriesAtom } from "@/store/diaryEntriesAtom";
+import { fakeDiaryEntries } from "@/utils/fakeDiaryEntries";
 
 export default function TabTwoScreen() {
+  const [_, setDiaryEntries] = useAtom(diaryEntriesAtom);
+
+  const onResetDiary = () => {
+    setDiaryEntries(fakeDiaryEntries);
+    Alert.alert(
+      "Diary reset",
+      "Your diary has been reset to the default state",
+      [
+        {
+          text: "OK",
+          onPress: () => router.push("/"),
+        },
+      ],
+    );
+  };
+
   return (
     <StyledSafeAreaView>
       <Text fontSize={24}>Settings</Text>
+      <Button icon={History} onPress={onResetDiary} scale={1.1}>
+        Reset diary
+      </Button>
     </StyledSafeAreaView>
   );
 }
