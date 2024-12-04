7186c5590df4c3b578ec92e094820933caf08f23
MattAgn <matthieua@bam.tech>
2024-12-04 22:35:18 +0100

    feat: add global error boundary

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,16 +4,48 @@ import "react-native-reanimated";
 import "react-native-get-random-values";
 
 import { useFonts } from "expo-font";
+import type { ErrorBoundaryProps } from "expo-router";
 import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { StatusBar } from "expo-status-bar";
+import { useAtom } from "jotai";
 import { useEffect } from "react";
-import { TamaguiProvider } from "tamagui";
+import { Alert } from "react-native";
+import { Button, Spacer, TamaguiProvider, Text, View } from "tamagui";
+
+import { diaryEntriesAtom } from "@/store/diaryEntriesAtom";
 
 import { tamaguiConfig } from "../tamagui.config";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 void SplashScreen.preventAutoHideAsync();
+
+export const ErrorBoundary = ({ error, retry }: ErrorBoundaryProps) => {
+  const [_, setDiaryEntries] = useAtom(diaryEntriesAtom);
+
+  const resetStorage = () => {
+    setDiaryEntries([]);
+    Alert.alert("Your diary has been reset");
+    void retry();
+  };
+
+  return (
+    <View
+      flex={1}
+      justifyContent="center"
+      alignItems="center"
+      backgroundColor={"red"}
+    >
+      <Text fontSize={"$7"}>Something went wrong</Text>
+      <Spacer scaleY={"$2"} />
+      <Text>{error.message}</Text>
+      <Spacer scaleY={"$2"} />
+      <Button onPress={retry}>Try Again?</Button>
+      <Spacer scaleY={"$2"} />
+      <Button onPress={resetStorage}>Empty storage and try again</Button>
+    </View>
+  );
+};
 
 export default function RootLayout() {
   const [loaded] = useFonts({
