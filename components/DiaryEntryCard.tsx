fc207e6a3f1fed63a37d513cfb0c32a12731a612
MattAgn <matthieua@bam.tech>
2024-12-05 23:41:53 +0100

    feat: make cards dark

--- components/DiaryEntryCard.tsx
+++ components/DiaryEntryCard.tsx
@@ -13,13 +13,16 @@ export const DiaryEntryCard = ({
 }) => {
   return (
     <Card
+      backgroundColor={"#3C3949"}
       p="$3"
       marginTop="$4"
       key={diaryEntry.id}
       onPress={() => router.push(`/diary-entry/${diaryEntry.id}`)}
     >
       <Card.Header flexDirection="row" justifyContent="space-between">
-        <Text fontSize={10}>{diaryEntry.createdAt.toLocaleDateString()}</Text>
+        <Text color={"white"} fontSize={10}>
+          {diaryEntry.createdAt.toLocaleDateString()}
+        </Text>
         <Button
           hitSlop={15}
           icon={Ellipsis}
@@ -30,9 +33,13 @@ export const DiaryEntryCard = ({
           onPress={() => onActionButtonPress(diaryEntry.id)}
         />
       </Card.Header>
-      <Text>{diaryEntry.title}</Text>
+      <Text color={"white"} fontSize={"$5"}>
+        {diaryEntry.title}
+      </Text>
       <Spacer scaleY={"$1"} />
-      <Text numberOfLines={3}>{diaryEntry.content}</Text>
+      <Text color={"white"} numberOfLines={3}>
+        {diaryEntry.content}
+      </Text>
       <Card.Footer></Card.Footer>
     </Card>
   );
