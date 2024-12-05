5bd46354a8fc828e18d53a6389267831ae43cfc9
MattAgn <matthieua@bam.tech>
2024-12-05 22:49:08 +0100

    feat: display gradient as background

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,10 @@
 import { PlusCircle } from "@tamagui/lucide-icons";
+import { LinearGradient } from "expo-linear-gradient";
 import { router } from "expo-router";
 import { useAtom } from "jotai";
 import { useMemo, useState } from "react";
 import { Alert } from "react-native";
-import {
-  SafeAreaView,
-  useSafeAreaInsets,
-} from "react-native-safe-area-context";
+import { SafeAreaView } from "react-native-safe-area-context";
 import { Button, Input, Spacer, styled, Text, View } from "tamagui";
 
 import { ActionSheet } from "@/components/ActionSheet";
@@ -19,7 +17,6 @@ import { Colors } from "@/constants/Colors";
 import { diaryEntriesAtom } from "@/store/diaryEntriesAtom";
 
 export default function HomeScreen() {
-  const { bottom } = useSafeAreaInsets();
   const [diaryEntries, setDiaryEntries] = useAtom(diaryEntriesAtom);
   const [searchQuery, setSearchQuery] = useState("");
   const [isSheetOpen, setIsSheetOpen] = useState(false);
@@ -71,47 +68,52 @@ export default function HomeScreen() {
   };
 
   return (
-    <StyledSafeAreaView marginBottom={bottom}>
-      <View flex={1} padding="$4" marginBottom={"$3"}>
-        <Text>My diary</Text>
-        <Spacer scaleY={"$1"} />
-        <Input
-          placeholder="Search"
-          value={searchQuery}
-          onChangeText={setSearchQuery}
+    <StyledLinearGradient colors={["#0E1020", "#5C3A65"]}>
+      <StyledSafeAreaView>
+        <View flex={1} padding="$4" marginBottom={"$3"}>
+          <Text>My diary</Text>
+          <Spacer scaleY={"$1"} />
+          <Input
+            placeholder="Search"
+            value={searchQuery}
+            onChangeText={setSearchQuery}
+          />
+          <DiaryEntriesList
+            entries={filteredAndSortedEntries}
+            onActionButtonPress={openActionSheet}
+            EmptyListComponent={
+              searchQuery !== "" ? (
+                <DiaryEntriesNoSearchResult />
+              ) : (
+                <DiaryNoEntriesYet />
+              )
+            }
+          />
+          <Spacer scaleY={"$2"} />
+          <Spacer scaleY={1} />
+          <Button
+            icon={PlusCircle}
+            color={Colors["light"].tint}
+            onPress={() => router.push("/new-diary-entry")}
+          >
+            New entry
+          </Button>
+        </View>
+        <ActionSheet
+          isSheetOpen={isSheetOpen}
+          setIsSheetOpen={setIsSheetOpen}
+          onDelete={showDeleteAlert}
+          onEdit={editEntry}
         />
-        <DiaryEntriesList
-          entries={filteredAndSortedEntries}
-          onActionButtonPress={openActionSheet}
-          EmptyListComponent={
-            searchQuery !== "" ? (
-              <DiaryEntriesNoSearchResult />
-            ) : (
-              <DiaryNoEntriesYet />
-            )
-          }
-        />
-        <Spacer scaleY={"$2"} />
-        <Spacer scaleY={1} />
-        <Button
-          icon={PlusCircle}
-          color={Colors["light"].tint}
-          onPress={() => router.push("/new-diary-entry")}
-        >
-          New entry
-        </Button>
-      </View>
-      <ActionSheet
-        isSheetOpen={isSheetOpen}
-        setIsSheetOpen={setIsSheetOpen}
-        onDelete={showDeleteAlert}
-        onEdit={editEntry}
-      />
-    </StyledSafeAreaView>
+      </StyledSafeAreaView>
+    </StyledLinearGradient>
   );
 }
 
 const StyledSafeAreaView = styled(SafeAreaView, {
   flex: 1,
-  backgroundColor: Colors["light"].background,
+});
+
+const StyledLinearGradient = styled(LinearGradient, {
+  flex: 1,
 });
