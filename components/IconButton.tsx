fdc30b8501b9b1d525bbd64e807d4f20258e2c70
MattAgn <matthieua@bam.tech>
2024-12-08 17:38:35 +0100

    fix: fix buttons responsiveness

--- components/IconButton.tsx
+++ components/IconButton.tsx
@@ -31,3 +31,20 @@ export const IconButton = styled(Button, {
     },
   },
 });
+
+/**
+ * Used on action buttons in the new diary entry screen and the diary-entry/[id] screen.
+ * For some unknown reason, the buttons don't behave properly (not always clickable)
+ * when using the styled(Button) directly. Seems also to be linked with the "transparent" pressed style but not sure
+ */
+export const iconActionButtonProps = {
+  color: "white",
+  size: "$8",
+  circular: true,
+  backgroundColor: "$darkLightBackground",
+  pressStyle: {
+    opacity: 0.5,
+    backgroundColor: "$greyPurple",
+    borderColor: "$greyPurple",
+  },
+};
